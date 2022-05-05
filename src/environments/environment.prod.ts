cb8702eefb5f771a5a9b75d67eef896ac1801cd5
SAMBIT SARGAM EKALABAYA <sambitsargam2003@gmail.com>
2022-05-06 01:00:46 +0530

    test

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    appName: 'musicfy',
+    appVersion: '0.1.0',
+    web3StorageToken:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDI1MjIzODBhQTEzODA5QTU0OEY4ZTc1ZDgzQTAwMTA3RTQ0YzcxNTgiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTE3Nzg0MzE1MDEsIm5hbWUiOiJtdXNpY2Z5In0.i5XhWXJ5ckGaBJW4rsatHk3BFwBnmnUEcEQr4-1GGDM',
+    ORBITDB_STORE_SONG: 'musicfy.songs',
+    ORBITDB_STORE_ALBUM: 'musicfy.albums',
+    ORBITDB_STORE_ARTIST: 'musicfy.artists',
+    ORBITDB_STORE_NEWS: 'musicfy.news',
+    ORBITDB_STORE_PLAYLIST: 'musicfy.playlists',
+    ORBITDB_STORE_PLAYLIST_SONG: 'musicfy.playlistsongs',
+    ORBITDB_STORE_LIKED_SONG: 'musicfy.likedsongs'
+};
